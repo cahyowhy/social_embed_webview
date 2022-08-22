a5591fcc562b1142fa426e7006cb2bcdd28e47ba
cahyowhy <cahyowhy01@gmail.com>
2022-08-22 21:37:23 +0700

    fixes : twitter profile add aditional data parameter

--- lib/platforms/twitter-profile.dart
+++ lib/platforms/twitter-profile.dart
@@ -3,9 +3,11 @@ import 'social-media-generic.dart';
 class TwitterProfileEmbedData extends SocialMediaGenericEmbedData {
   final String username;
   final double? aspectRatio;
+  final String? theme;
+  final int? limit;
 
   const TwitterProfileEmbedData(
-      {required this.username, this.aspectRatio = 3 / 4})
+      {required this.username, this.aspectRatio = 3 / 4, this.theme, this.limit})
       : super(aspectRatio: aspectRatio);
 
   @override
@@ -13,7 +15,7 @@ class TwitterProfileEmbedData extends SocialMediaGenericEmbedData {
 
   @override
   String get htmlBody => """
-    <a class="twitter-timeline" data-theme="light" href="https://twitter.com/$username?ref_src=twsrc%5Etfw"></a>
+    <a class="twitter-timeline" data-limit="${limit ?? 15}" data-theme="${theme ?? 'light'}" href="https://twitter.com/$username?ref_src=twsrc%5Etfw"></a>
     <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
   """;
 }
